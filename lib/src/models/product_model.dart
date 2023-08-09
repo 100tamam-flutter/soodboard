e91f795182bb6adcb1a88d3fdd8e80d899bae0ae
manirahmanzadeh <72001950+manirahmanzadeh@users.noreply.github.com>
2023-08-10 00:06:33 +0330

    imp api

--- lib/src/models/product_model.dart
+++ lib/src/models/product_model.dart
@@ -1,25 +1,33 @@
+import 'package:soodboard/src/models/product_rate_model.dart';
+
 class ProductModel {
   final String id;
   final String title;
+  final String description;
+  final String category;
+  final ProductRateModel? rate;
   final String image;
   final double price;
-  final int discount;
 
   const ProductModel({
     required this.id,
     required this.title,
+    required this.description,
+    required this.category,
+    required this.rate,
     required this.image,
     required this.price,
-    required this.discount,
   });
 
   factory ProductModel.fromJson(Map<String, dynamic> json) {
     return ProductModel(
-      id: json['id'] as String,
+      id: json['id'].toString(),
       title: json['title'] as String,
+      description: json['description'] as String,
+      category: json['category'] as String,
+      rate: json['rate'] != null  ? ProductRateModel.fromJson(json['rate'] as Map<String, dynamic>) :null,
       image: json['image'] as String,
-      price: json['price'] as double,
-      discount: json['discount'] as int,
+      price: double.parse(json['price'].toString()) ,
     );
   }
 }
--- lib/src/modules/favorite/api/favorite_products_api.dart
+++ lib/src/modules/favorite/api/favorite_products_api.dart
@@ -2,6 +2,10 @@ import 'dart:convert';
 import 'package:flutter/services.dart';
 import 'package:soodboard/src/models/product_model.dart';
 
+import '../../../constants/urls.dart';
+import '../../../core/api/core_api.dart';
+import '../../../models/error_template.dart';
+
 abstract class FavoriteProductsAPI {
   Future<List<ProductModel>> getProducts() async {
     throw UnimplementedError();
@@ -27,4 +31,27 @@ class FavoriteProductsAPIMock implements FavoriteProductsAPI {
         .map((e) => ProductModel.fromJson(e as Map<String, dynamic>))
         .toList();
   }
+}
+
+class FavoriteProductsApiImp implements FavoriteProductsAPI {
+
+  final _coreApi = CoreApi();
+  @override
+  Future<List<ProductModel>> getProducts() async {
+    final response = await _coreApi.get(
+      Uri.parse(
+        Urls.getProducts,
+      ),
+    );
+    if (response == null) {
+      throw ApiError(message: 'مشکل در ارتباط با سرور');
+    }
+    final responseBody =
+    jsonDecode(utf8.decode(response.bodyBytes)) as List;
+    if (response.statusCode != 200) {
+      throw ApiError(message: 'خطا');
+    }
+    return responseBody.map((e) => ProductModel.fromJson(e)).toList();
+  }
+
 }
