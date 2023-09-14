196b5f6d115aec926bea9be8fa801be5905bb189
manirahmanzadeh <72001950+manirahmanzadeh@users.noreply.github.com>
2023-09-14 20:53:58 +0330

    merged with dev

--- lib/src/modules/login/pages/login_page.dart
+++ lib/src/modules/login/pages/login_page.dart
@@ -9,11 +9,11 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: ListView(
-          children:[
+          children: [
             const SizedBox(
               height: 68,
             ),
@@ -31,123 +31,116 @@ class LoginPage extends StatelessWidget {
                 color: Color(0xFF223263),
               ),
             ),
-              const SizedBox(
-                height: 8,
-              ),
-              Text(
-          'Sign in to continue',
-          textAlign: TextAlign.center,
-          style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-            color: const Color(0xFF9098B1),
-            letterSpacing: 0.5,
-          ),
-        ),
-          const SizedBox(
-            height: 28,
-          ),
-
-             const TextFieldComponent(title:'Your Email',
-              icon: 'assets/icons/message.svg',
-              ),
-
-
-           const SizedBox(
-             height: 8,
-           ),
-
-              const TextFieldComponent(title:'Password',
-               icon: 'assets/icons/password.svg',
-              ),
-
-
-           const SizedBox(
-             height: 16,
-           ),
-           const SignInButtonComponent(),
-
+            const SizedBox(
+              height: 8,
+            ),
+            Text(
+              'Sign in to continue',
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                    color: const Color(0xFF9098B1),
+                    letterSpacing: 0.5,
+                  ),
+            ),
+            const SizedBox(
+              height: 28,
+            ),
+            const TextFieldComponent(
+              title: 'Your Email',
+              icon: 'assets/icons/email.svg',
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            const TextFieldComponent(
+              title: 'Password',
+              icon: 'assets/icons/passphrase.svg',
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            const SignInButtonComponent(),
             const SizedBox(
               height: 21,
             ),
             Row(
-              mainAxisAlignment:MainAxisAlignment.center,
-              children:[
-               Container(
-                 height: 1,
-                 width: MediaQuery.of(context).size.width/3,
-                 color: const Color(0xFFEBF0FF),
-               ),
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 1,
+                  width: MediaQuery.of(context).size.width / 3,
+                  color: const Color(0xFFEBF0FF),
+                ),
                 Padding(
-                   padding:
-               const EdgeInsets.symmetric(horizontal: 28),
-                  child: Text("OR",
-                   style: Theme.of(context).textTheme.titleSmall!.copyWith(
-                    color: const Color(0xFF9098B1),
-                     letterSpacing: 0.7,
-                   ),
-                 ),
+                  padding: const EdgeInsets.symmetric(horizontal: 28),
+                  child: Text(
+                    "OR",
+                    style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                          color: const Color(0xFF9098B1),
+                          letterSpacing: 0.7,
+                        ),
+                  ),
                 ),
                 Container(
                   height: 1,
-                  width: MediaQuery.of(context).size.width/3,
+                  width: MediaQuery.of(context).size.width / 3,
                   color: const Color(0xFFEBF0FF),
                 ),
               ],
             ),
-           const SizedBox(
-             height: 16,
-           ),
-           const LogInButtonComponent(
-             icon: 'assets/icons/google.svg',
-               title:'Login with Google',
-           ),
             const SizedBox(
-              height:8,
+              height: 16,
             ),
-        const LogInButtonComponent(
-                   icon: 'assets/icons/facebook.svg',
-                  title:'Login with facebook'
-        ),
-
+            const LogInButtonComponent(
+              icon: 'assets/icons/google.svg',
+              title: 'Login with Google',
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            const LogInButtonComponent(
+                icon: 'assets/icons/facebook.svg',
+                title: 'Login with facebook'),
             const SizedBox(
               height: 16,
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text('Forgot Password?',
+                Text(
+                  'Forgot Password?',
                   style: Theme.of(context).textTheme.labelLarge!.copyWith(
-                    color: const Color(0xFF40BFFF),
-                    letterSpacing: 0.5,
-                  ),
+                        color: const Color(0xFF40BFFF),
+                        letterSpacing: 0.5,
+                      ),
                 ),
               ],
             ),
-
             const SizedBox(
               height: 8,
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text('Don’t have a account?',
+                Text(
+                  'Don’t have a account?',
                   style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                    color: const Color(0xFF9098B1),
-                    letterSpacing: 0.5,
-                  ),
+                        color: const Color(0xFF9098B1),
+                        letterSpacing: 0.5,
+                      ),
                 ),
-                Text(' Register',
+                Text(
+                  ' Register',
                   style: Theme.of(context).textTheme.labelLarge!.copyWith(
-                    color: const Color(0xFF40BFFF),
-                    letterSpacing: 0.5,
-                  ),
+                        color: const Color(0xFF40BFFF),
+                        letterSpacing: 0.5,
+                      ),
                 ),
               ],
             ),
-
           ],
         ),
       ),
-
     );
   }
 }
