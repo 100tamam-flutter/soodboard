bbfe47dff6c2c8307edf7dbe2247ab9e53fcd2f7
manirahmanzadeh <72001950+manirahmanzadeh@users.noreply.github.com>
2023-09-14 20:31:49 +0330

    fixed password page ui

--- lib/src/modules/profile/pages/changepasword_page.dart
+++ lib/src/modules/profile/pages/changepasword_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:vrouter/vrouter.dart';
 import '../providers/changepasword_provider.dart';
-import '../providers/phonenumbre_provider.dart';
 
 
 class ChangePassword extends StatelessWidget {
@@ -30,13 +29,9 @@ class _ChangePassword extends StatelessWidget {
           onPressed: () => context.vRouter.pop(),
           icon: const Icon(Icons.arrow_back_ios),
         ),
+        elevation: 0,
         title: const Text(
-          'Change Password',
-          style: TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.w700,
-            color: Color(0xFF223263),
-          ),
+          'Phone Number',
         ),
       ),
       body: Padding(
@@ -44,75 +39,152 @@ class _ChangePassword extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Divider(
-              color: Color(0xFFEBF0FF),
-            ),
-            Text(
-              'Old Password',
-                style: Theme.of(context).textTheme.labelLarge,
-    ),
-            const SizedBox(
-              height: 12,
-            ),
-            const TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(
-              height: 12,
-            ),
-            Text(
-              'New Password',
-              style: Theme.of(context).textTheme.labelLarge,
-            ),
-            const SizedBox(
-              height: 12,
-            ),
-            const TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(
-              height: 12,
-            ),
-            Text(
-              'New Password Again',
-              style: Theme.of(context).textTheme.labelLarge,
-            ),
-            const SizedBox(
-              height: 12,
-            ),
-            const TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(
-              height: 300,
-            ),
-            ElevatedButton(
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.blue),
-                foregroundColor: MaterialStateProperty.all(Colors.white),
-                shape: MaterialStateProperty.all(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                ),
-              ),
-              onPressed: () {},
-              child: const Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+            Expanded(
+              child: ListView(
                 children: [
-                  Padding(
-                    padding: EdgeInsets.all(16),
-                    child: Text('Save'),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Text(
+                    'Old Password',
+                    style: Theme.of(context).textTheme.titleSmall,
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  TextField(
+                    obscureText: true,
+                    style: Theme.of(context).textTheme.labelLarge,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5),
+                        borderSide: const BorderSide(color: Color(0xFFEBF0FF)),
+                      ),
+                      disabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5),
+                        borderSide: const BorderSide(color: Color(0xFFEBF0FF)),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5),
+                        borderSide: const BorderSide(color: Color(0xFFEBF0FF)),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5),
+                        borderSide: const BorderSide(color: Color(0xFFEBF0FF)),
+                      ),
+                      hintStyle: Theme.of(context).textTheme.labelLarge,
+                      hintText: '.................',
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 24,
+                  ),
+
+                  Text(
+                    'New Password',
+                    style: Theme.of(context).textTheme.titleSmall,
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  TextField(
+                    obscureText: true,
+                    style: Theme.of(context).textTheme.labelLarge,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5),
+                        borderSide: const BorderSide(color: Color(0xFFEBF0FF)),
+                      ),
+                      disabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5),
+                        borderSide: const BorderSide(color: Color(0xFFEBF0FF)),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5),
+                        borderSide: const BorderSide(color: Color(0xFFEBF0FF)),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5),
+                        borderSide: const BorderSide(color: Color(0xFFEBF0FF)),
+                      ),
+                      hintStyle: Theme.of(context).textTheme.labelLarge,
+                      hintText: '.................',
+                    ),
+                  ),
+
+                  const SizedBox(
+                    height: 24,
+                  ),
+
+                  Text(
+                    'New Password Again',
+                    style: Theme.of(context).textTheme.titleSmall,
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  TextField(
+                    obscureText: true,
+                    style: Theme.of(context).textTheme.labelLarge,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5),
+                        borderSide: const BorderSide(color: Color(0xFFEBF0FF)),
+                      ),
+                      disabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5),
+                        borderSide: const BorderSide(color: Color(0xFFEBF0FF)),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5),
+                        borderSide: const BorderSide(color: Color(0xFFEBF0FF)),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5),
+                        borderSide: const BorderSide(color: Color(0xFFEBF0FF)),
+                      ),
+                      hintStyle: Theme.of(context).textTheme.labelLarge,
+                      hintText: '.................',
+                    ),
                   ),
                 ],
               ),
             ),
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.blue),
+                    foregroundColor: MaterialStateProperty.all(Colors.white),
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(16),
+                        child: Text(
+                          'Save',
+                          style:
+                          Theme.of(context).textTheme.titleSmall!.copyWith(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+              ],
+            )
           ],
         ),
       ),
