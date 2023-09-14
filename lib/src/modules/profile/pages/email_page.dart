f2e39cd6b9742a86263bc3025840b1e643b9be5b
manirahmanzadeh <72001950+manirahmanzadeh@users.noreply.github.com>
2023-09-14 20:26:02 +0330

    fixed email page ui

--- lib/src/modules/profile/pages/email_page.dart
+++ lib/src/modules/profile/pages/email_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:vrouter/vrouter.dart';
-import '../providers/birthday_provider.dart';
 import '../providers/email_provider.dart';
 
 
@@ -31,60 +30,100 @@ class _EmailPage extends StatelessWidget {
           onPressed: () => context.vRouter.pop(),
           icon: const Icon(Icons.arrow_back_ios),
         ),
+        elevation: 0,
         title: const Text(
           'Email',
-          style: TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.w700,
-            color: Color(0xFF223263),
-          ),
         ),
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            const Divider(
-              color: Color(0xFFEBF0FF),
-            ),
-            Text(
-              'Change Email',
-              style: Theme.of(context).textTheme.labelLarge,
-            ),
-            const SizedBox(
-              height: 12,
-            ),
-            TextField(
-              decoration: InputDecoration(
-                border: const OutlineInputBorder(),
-                hintText: staticProvider.email,
-              ),
-            ),
-            const SizedBox(
-              height: 486,
-            ),
-            ElevatedButton(
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.blue),
-                foregroundColor: MaterialStateProperty.all(Colors.white),
-                shape: MaterialStateProperty.all(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 16,
+                ),
+                Text(
+                  'Change Email',
+                  style: Theme.of(context).textTheme.titleSmall,
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                TextField(
+                  style: Theme.of(context).textTheme.labelLarge,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                      borderSide: const BorderSide(color: Color(0xFFEBF0FF)),
+                    ),
+                    disabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                      borderSide: const BorderSide(color: Color(0xFFEBF0FF)),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                      borderSide: const BorderSide(color: Color(0xFFEBF0FF)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                      borderSide: const BorderSide(color: Color(0xFFEBF0FF)),
+                    ),
+                    hintStyle: Theme.of(context).textTheme.labelLarge,
+                    hintText: staticProvider.email,
                   ),
                 ),
-              ),
-              onPressed: () {},
-              child: const Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.all(16),
-                    child: Text('Save'),
+                const SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  'We Will Send verification to your New Email',
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                    color: const Color(0xFF40BFFF)
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.blue),
+                    foregroundColor: MaterialStateProperty.all(Colors.white),
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(16),
+                        child: Text(
+                          'Save',
+                          style:
+                          Theme.of(context).textTheme.titleSmall!.copyWith(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+              ],
+            )
           ],
         ),
       ),
