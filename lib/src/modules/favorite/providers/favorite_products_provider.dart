beb1e1129e3ba7b899807f9adcc88f560e6ff51f
manirahmanzadeh <72001950+manirahmanzadeh@users.noreply.github.com>
2023-12-04 02:03:54 +0330

    feat: favorite products and notifications animation

--- lib/src/modules/favorite/providers/favorite_products_provider.dart
+++ lib/src/modules/favorite/providers/favorite_products_provider.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:soodboard/src/models/product_model.dart';
 import 'package:soodboard/src/modules/favorite/api/favorite_products_api.dart';
 
+import '../../../components/product/product_tile_half_width.dart';
 import '../../../core/providers/safe_provider.dart';
 import '../../../models/error_template.dart';
 import '../../../utils/error_handler.dart';
@@ -22,16 +23,57 @@ class FavoriteProductsProvider extends SafeProvider with ErrorHandler {
     getProducts();
   }
 
+  GlobalKey<AnimatedGridState>? gridKey;
+
+  setGridKey(GlobalKey<AnimatedGridState> newKey) {
+    gridKey = newKey;
+  }
+
+  List<int> processes = [];
+
   Future<void> getProducts() async {
+    final previousItemsLength = products.length;
+    final List<ProductModel> copyOfProducts = List.from(products);
+
+    for (int i = previousItemsLength - 1; i >= 0; i--) {
+      gridKey!.currentState!.removeItem(
+        i,
+        (context, animation) => ScaleTransition(
+          scale: animation,
+          child: Center(
+            child: ProductTileHalfWidth(
+              productModel: copyOfProducts[i],
+            ),
+          ),
+        ),
+      );
+
+      // Remove item from the original list
+      products.removeAt(i);
+      notifyListeners();
+    }
     loadingProducts = true;
     notifyListeners();
     try {
-      products = await _favoriteProductsApi.getProducts();
+      processes.add(0);
+      final newProducts = await _favoriteProductsApi.getProducts();
+      processes.removeLast();
+      if (processes.isEmpty) {
+        // only the last process can modify the list
+        loadingProducts = false;
+        notifyListeners();
+        for (int i = 0; i < newProducts.length; i++) {
+          products.add(newProducts[i]);
+          notifyListeners();
+          gridKey!.currentState!.insertItem(i, duration: const Duration(milliseconds: 350));
+          await Future.delayed(const Duration(milliseconds: 100));
+        }
+      }
     } on ApiError catch (e) {
       showError(context, e);
+      loadingProducts = false;
+      notifyListeners();
     }
-    loadingProducts = false;
-    notifyListeners();
   }
 
   removeItem(int index) {
@@ -61,6 +103,19 @@ class FavoriteProductsProvider extends SafeProvider with ErrorHandler {
       ),
     );
     if (remove) {
+      final List<ProductModel> copyOfProducts = List.from(products);
+
+      gridKey!.currentState!.removeItem(
+        index,
+        (context, animation) => ScaleTransition(
+          scale: animation,
+          child: Center(
+            child: ProductTileHalfWidth(
+              productModel: copyOfProducts[index],
+            ),
+          ),
+        ),
+      );
       products.removeAt(index);
       notifyListeners();
     }
